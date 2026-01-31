abdb8ed7c9fa5a3c0e438551da2925327051301f
Vardhan <vardhandondolly123456@gmail.com>
2026-01-31 14:34:51 +0530

    Add files via upload

---
+++ Big.java
@@ -0,0 +1,16 @@
+class Big
+{
+    public static void main(String[] args)
+    {
+        int a=03;
+        int b=24;
+        if(a>b)
+        {
+            System.out.println("a is big");
+        }
+        else
+        {
+            System.out.println("b is big");
+        }
+    }
+}
