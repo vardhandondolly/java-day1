12014cb76fb42ddf661b2c33ce9579ee75f2591b
Vardhan <vardhandondolly123456@gmail.com>
2026-01-31 14:35:14 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,9 @@
+class Add
+{
+    public static void main(String [] args)
+    {
+        System.out.println("Hello World");
+        System.out.print("Hi");
+        System.out.print("Hello");
+    }
+}
