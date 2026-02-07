37c082671bd32e4a81407e9ca54a5a61bb7c8cb8
Vardhan <vardhandondolly123456@gmail.com>
2026-02-07 15:13:48 +0530

    Add files via upload

---
+++ Swaping.java
@@ -0,0 +1,13 @@
+class Swaping
+{
+    public static void main(String args[])
+    {
+        int a=34;
+        int b=45;
+        int temp=a;
+         a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
