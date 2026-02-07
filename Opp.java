02c9e15ab025fd283695ddf324c6aa9b150cd508
Vardhan <vardhandondolly123456@gmail.com>
2026-02-07 14:32:24 +0530

    Add files via upload

---
+++ Opp.java
@@ -0,0 +1,40 @@
+class Opp
+{
+    public static void main(String args[])
+    {
+        int a=24;
+        int b=30;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println(+a);
+        System.out.println(-b);
+        System.out.println(a++);
+        System.out.println(++b);
+        System.out.println(b--);
+        System.out.println(--a);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println(a&b);
+        System.out.println(a^b);
+        System.out.println(~a);
+        System.out.println(a|b);
+        System.out.println(a>b&&a<b);
+        System.out.println(a>b||a<b);
+        System.out.println(a=b);
+        System.out.println(a+=b);
+        System.out.println(a-=b);
+        System.out.println(a*=b);
+        System.out.println(a/=b);
+        System.out.println(a%=b);
+        System.out.println(a<<6);
+        System.out.println(b>>6);
+
+    }
+}
