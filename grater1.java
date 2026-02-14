ee78f62589c1d8f11b8d3745cb79b3a4bce5d53e
Vardhan <vardhandondolly123456@gmail.com>
2026-02-14 12:04:14 +0530

    Add files via upload

---
+++ grater1.java
@@ -0,0 +1,17 @@
+import java.util.*; 
+public class grater1
+{
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first number");
+        int a=sc.nextInt();
+        System.out.println("Enter second number");
+        int b=sc.nextInt();
+        System.out.println("Enter third number");
+        int c=sc.nextInt();
+        if(a>b&&a>c)System.out.println("A is Grater");
+        else if(b>a&&b>c)System.out.println("B is Grater");
+        else System.out.println("C is Grater");
+    }
+}
